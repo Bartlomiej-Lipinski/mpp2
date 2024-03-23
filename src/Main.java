6cb1f02258ecf82377e24086a8c08416bc24efba
rochmisz <109878606+rochmisz@users.noreply.github.com>
2024-03-23 21:18:47 +0100

    skończone

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,111 @@
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    private static Perceptron perceptron;
+    private static PerceptronSetosa perceptron;
+    public static double setosa = 0;
+    public static int versicolor = 0;
+    public static boolean isTrained = false;
+    private static int testaccuracy = 0;
+    private static int n = 0;
+    public static boolean firstloop = true;
+    
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        perceptron = new PerceptronSetosa(n);
+        while (!isTrained) {
+            readFromFile("Train-set.txt");
+        }
+        readFromFile("Test-set.txt");
+        System.out.println("Setosa: " + setosa);
+        System.out.println("Versicolor: " + versicolor);
+        System.out.println("Accuracy : " +  setosa / 15 * 100 + "%");
+        ////////////////////////////////////////////////////////////////////////////////////////////
+        while (true) {
+            System.out.println("Podaj vectory separowane przecinkiem np: v1,v2,v3,v4");
+            System.out.println("Aby zakończyć wpisz exit");
+            ArrayList<Double> vec = new ArrayList<>();
+            String vektorek = scanner.next();
+            if (vektorek.equals("exit")) {
+                break;
+            }
+            String[] split = vektorek.split(",");
+            for (String d : split) {
+                double v = Double.parseDouble(d);
+                vec.add(v);
+            }
+            int answer = perceptron.guess(vec);
+            if (answer == 1) {
+                System.out.println("Iris-setosa");
+            } else {
+                System.out.println("Iris-versicolor");
+            }
+        }
     }
     public static void readFromFile(String path) {
         File file = new File(path);
+        testaccuracy = 0;
         try {
             Scanner s = new java.util.Scanner(file);
+            ArrayList<Double> args = new ArrayList<>();
             while (s.hasNextLine()) {
                 String line = s.nextLine();
                 String[] split = line.split(",");
-                double v1 = Double.parseDouble(split[0]);
-                double v2 = Double.parseDouble(split[1]);
-                double v3 = Double.parseDouble(split[2]);
-                double v4 = Double.parseDouble(split[3]);
-                String name = split[4];
-                if (path.equals("Train-set.txt")) {
-                    perceptron.train(name, v1, v2, v3, v4);
+                for (int i =0;i<split.length-1;i++){
+                    double v = Double.parseDouble(split[i]);
+                    args.add(v);
                 }
+                String result = split[split.length-1];
+                if (firstloop) {
+                    n = split.length-1;
+                    perceptron.changeArgs(n);
+                    firstloop = false;
+                }
+                ////////////////////////////////////////////////////////////////////////
+                if (path.equals("Train-set.txt")) {
+                   int setos = perceptron.guess(args);
+                   if (result.equals("Iris-setosa")&& setos ==1) {
+                       testaccuracy++;
+                   }
+                    if (result.equals("Iris-setosa")&& setos ==0) {
+                        testaccuracy--;
+                    }
+                   if (result.equals("Iris-versicolor")&& setos == 0) {
+                       testaccuracy++;
+                   }
+                    if (result.equals("Iris-versicolor")&& setos == 1) {
+                        testaccuracy--;
+                    }
+                    if (testaccuracy == 70) {
+                        isTrained = true;
+                    }
+                    if (result.equals("Iris-setosa")) {
+                        if (setos == 1) {
+                            args.clear();
+                            continue;
+                        }
+                        perceptron.train(args, 1);
+                    } else {
+                        if (setos == 0) {
+                            args.clear();
+                            continue;
+                        }
+                        perceptron.train(args, 0);
+                    }
+                    args.clear();
+                }
+                ////////////////////////////////////////////////////////////////////////////////////////////
                 if (path.equals("Test-set.txt")) {
-                    perceptron.guess(name, v1, v2, v3, v4);
+                    int resultGuess = perceptron.guess(args);
+                    if (resultGuess == 1) {
+                        System.out.println("Iris-setosa");
+                        setosa++;
+                    } else {
+                        System.out.println("Iris-versicolor");
+                        versicolor++;
+                    }
+                    args.clear();
                 }
             }
         } catch (Exception e) {
